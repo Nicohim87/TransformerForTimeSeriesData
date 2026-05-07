abb3f1a86878023e109c5bd340db047c59098c35
Nicohim87 <nicohim8704@gmail.com>
2026-05-07 11:00:03 +0700

    fix: transformer architecture mistakes and implementing dropout

--- transformer_architecture.py
+++ transformer_architecture.py
@@ -5,20 +5,14 @@ import copy
 import math
 
 class DotProductAttention(nn.Module):
-    def __init__(self, hidden_dim, mask=False):
+    def __init__(self, hidden_dim, dropout, mask=False):
         super(DotProductAttention, self).__init__()
         self.hidden_dim = hidden_dim
         self.mask = mask
-
-        self.fc_q = nn.Linear(hidden_dim, hidden_dim)
-        self.fc_k = nn.Linear(hidden_dim, hidden_dim)
-        self.fc_v = nn.Linear(hidden_dim, hidden_dim)
+        self.dropout = nn.Dropout(dropout)
 
 
     def forward(self, query, key, value):
-        query = self.fc_q(query)
-        key = self.fc_k(key)
-        value = self.fc_v(value)
 
         score = torch.bmm(query, key.transpose(1, 2)) # (batch, d_seq, h_dim) * (batch, h_dim, e_seq) = (batch, d_seq, e_seq)
         score /= math.sqrt(query.shape[-1])
@@ -30,6 +24,7 @@ class DotProductAttention(nn.Module):
 
         # score -> (batch, d_seq, e_seq)
         score = torch.softmax(score, dim=-1) # Softmax along e_seq
+        score = self.dropout(score)
 
 
         context_vector = torch.bmm(score, value) # (batch, d_seq, e_seq) * (batch, e_seq, h_dim) = (batch, d_seq, h_dim)
@@ -37,7 +32,7 @@ class DotProductAttention(nn.Module):
   
 
 class MultiheadAttention(nn.Module):
-    def __init__(self, n_heads, hidden_dim, mask=False):
+    def __init__(self, n_heads, hidden_dim, dropout, mask=False):
         super().__init__()
 
         assert hidden_dim % n_heads == 0
@@ -46,9 +41,14 @@ class MultiheadAttention(nn.Module):
         self.hidden_dim = hidden_dim
         self.head_dim = hidden_dim // n_heads
         self.mask = mask
+    
+        self.fc_q = nn.Linear(hidden_dim, hidden_dim)
+        self.fc_k = nn.Linear(hidden_dim, hidden_dim)
+        self.fc_v = nn.Linear(hidden_dim, hidden_dim)
+        self.dropout = nn.Dropout(dropout)
 
         self.attention = nn.ModuleList(
-            [DotProductAttention(self.head_dim, mask) for _ in range(n_heads)]
+            [DotProductAttention(self.head_dim, dropout, mask) for _ in range(n_heads)]
         )
 
         self.fc_cat = nn.Linear(hidden_dim, hidden_dim)
@@ -56,6 +56,10 @@ class MultiheadAttention(nn.Module):
     def forward(self, query, key, value):
 
         head_outputs = []
+
+        query = self.fc_q(query)
+        key = self.fc_k(key)
+        value = self.fc_v(value)
 
         for i, attention_head in enumerate(self.attention):
 
@@ -67,18 +71,20 @@ class MultiheadAttention(nn.Module):
 
         context_vector = torch.cat(head_outputs, dim=2)
 
-        return self.fc_cat(context_vector)
+        return self.dropout(self.fc_cat(context_vector))
     
 
 class PositionalFF(nn.Module):
-    def __init__(self, hidden_dim, ff_dim):
+    def __init__(self, hidden_dim, ff_dim, dropout):
         super(PositionalFF, self).__init__()
         self.fc1 = nn.Linear(hidden_dim, ff_dim)
-        self.activation = nn.ReLU()
+        self.activation = nn.GELU()
+        self.dropout = nn.Dropout(dropout)
         self.fc2 = nn.Linear(ff_dim, hidden_dim)
 
     def forward(self, values):
         values = self.activation(self.fc1(values))
+        values = self.dropout(values)
         return self.fc2(values)
     
 
@@ -102,52 +108,63 @@ class PositionalEncoding(nn.Module):
 
 
 class EncoderBlock(nn.Module):
-    def __init__(self, n_heads, hidden_dim):
+    def __init__(self, n_heads, hidden_dim, dropout):
         super(EncoderBlock, self).__init__()
         
-        self.multihead_attention = MultiheadAttention(n_heads, hidden_dim)
-        self.feed_forward = PositionalFF(hidden_dim, int(hidden_dim*4))
+        self.multihead_attention = MultiheadAttention(n_heads, hidden_dim, dropout)
+        self.feed_forward = PositionalFF(hidden_dim, int(hidden_dim*4), dropout)
         
-        self.norm1 = nn.LayerNorm(hidden_dim)
-        self.norm2 = nn.LayerNorm(hidden_dim)
+        self.norm_attn = nn.LayerNorm(hidden_dim)
+        self.norm_ff = nn.LayerNorm(hidden_dim)
+
+        self.dropout_attn = nn.Dropout(dropout)
+        self.dropout_ff = nn.Dropout(dropout)
     
     def forward(self, x):
-        attention = self.multihead_attention(x, x, x)
-        x = self.norm1(x + attention)
+        values = self.norm_attn(x)
+        attention = self.multihead_attention(values, values, values)
+        x = x + self.dropout_attn(attention)
 
-        ff = self.feed_forward(x)
-        x = self.norm2(x + ff)
+        ff = self.feed_forward(self.norm_ff(x))
+        x = x + self.dropout_ff(ff)
 
         return x
     
 class DecoderBlock(nn.Module):
-    def __init__(self, n_heads, hidden_dim):
+    def __init__(self, n_heads, hidden_dim, dropout):
         super(DecoderBlock, self).__init__()
 
-        self.input_attention = MultiheadAttention(n_heads, hidden_dim, mask=True)
-        self.context_attention = MultiheadAttention(n_heads, hidden_dim)
-        self.feed_forward = PositionalFF(hidden_dim, int(hidden_dim*4))
+        self.input_attention = MultiheadAttention(n_heads, hidden_dim, dropout, mask=True)
+        self.context_attention = MultiheadAttention(n_heads, hidden_dim, dropout)
+        self.feed_forward = PositionalFF(hidden_dim, int(hidden_dim*4), dropout)
 
-        self.norm1 = nn.LayerNorm(hidden_dim)
-        self.norm2 = nn.LayerNorm(hidden_dim)
-        self.norm3 = nn.LayerNorm(hidden_dim)
+        self.norm_selfattn = nn.LayerNorm(hidden_dim)
+        self.norm_ctxattn = nn.LayerNorm(hidden_dim)
+        self.norm_ff = nn.LayerNorm(hidden_dim)
+
+        self.dropout_selfattn = nn.Dropout(dropout)
+        self.dropout_ctxattn = nn.Dropout(dropout)
+        self.dropout_ff = nn.Dropout(dropout)
     
     def forward(self, values, context_vector=None):
-        input_attention = self.input_attention(values, values, values)
-        values = self.norm1(values + input_attention)
+        norm_values = self.norm_selfattn(values)
+        input_attention = self.input_attention(norm_values, norm_values, norm_values)
+        values = values + self.dropout_selfattn(input_attention)
 
+        norm_values = self.norm_ctxattn(values)
         if context_vector is not None:
-            context_attention = self.context_attention(values, context_vector, context_vector)
+            norm_context = self.norm_ctxattn(context_vector)
+            context_attention = self.context_attention(norm_values, norm_context, norm_context)
         else:
-            context_attention = self.context_attention(values, values, values)
-        values = self.norm2(values + context_attention)
+            context_attention = self.context_attention(norm_values, norm_values, norm_values)
+        values = values + self.dropout_ctxattn(context_attention)
 
-        ff = self.feed_forward(values)
-        values = self.norm3(values + ff)
+        ff = self.feed_forward(self.norm_ff(values))
+        values = values + self.dropout_ff(ff)
         return values
 
 class Encoder(nn.Module):
-    def __init__(self, corpus_size, hidden_dim, seq_len, n_blocks, n_heads, use_embedding=True, embedding_replacement:nn.Module=None):
+    def __init__(self, corpus_size, hidden_dim, seq_len, n_blocks, n_heads, dropout=0.1, use_embedding=True, embedding_replacement:nn.Module=None):
         super(Encoder, self).__init__()
         if use_embedding:
             self.embedding = nn.Embedding(corpus_size, hidden_dim)
@@ -155,7 +172,7 @@ class Encoder(nn.Module):
             self.embedding = embedding_replacement
         self.positional_encoding = PositionalEncoding(hidden_dim, seq_len)
         self.blocks = nn.ModuleList(
-            [EncoderBlock(n_heads, hidden_dim) for _ in range(n_blocks)]
+            [EncoderBlock(n_heads, hidden_dim, dropout) for _ in range(n_blocks)]
         )
 
     def forward(self, values):
@@ -169,7 +186,7 @@ class Encoder(nn.Module):
     
 
 class Decoder(nn.Module):
-    def __init__(self, corpus_size, hidden_dim, seq_len, n_blocks, n_heads, use_embedding=True, embedding_replacement:nn.Module=None):
+    def __init__(self, corpus_size, hidden_dim, seq_len, n_blocks, n_heads, dropout=0.1, use_embedding=True, embedding_replacement:nn.Module=None):
         super(Decoder, self).__init__()
         if use_embedding:
             self.embedding = nn.Embedding(corpus_size, hidden_dim)
@@ -177,7 +194,7 @@ class Decoder(nn.Module):
             self.embedding = embedding_replacement
         self.positional_encoding = PositionalEncoding(hidden_dim, seq_len)
         self.blocks = nn.ModuleList(
-            [DecoderBlock(n_heads, hidden_dim) for _ in range(n_blocks)]
+            [DecoderBlock(n_heads, hidden_dim, dropout) for _ in range(n_blocks)]
         )
 
         self.head = nn.Linear(hidden_dim, corpus_size)
@@ -193,10 +210,10 @@ class Decoder(nn.Module):
 
 
 class Transformer(nn.Module):
-    def __init__(self, hidden_dim, seq_len, corpus_size, encoder_blocks, encoder_attention_heads, decoder_blocks, decoder_attention_heads, use_embedding=True, embedding_replacement:nn.Module=None):
+    def __init__(self, hidden_dim, seq_len, corpus_size, encoder_blocks, encoder_attention_heads, decoder_blocks, decoder_attention_heads, dropout=0.1, use_embedding=True, embedding_replacement:nn.Module=None):
         super(Transformer, self).__init__()
-        self.encoder = Encoder(corpus_size, hidden_dim, seq_len, encoder_blocks, encoder_attention_heads, use_embedding, embedding_replacement)
-        self.decoder = Decoder(corpus_size, hidden_dim, seq_len, decoder_blocks, decoder_attention_heads, use_embedding, embedding_replacement)
+        self.encoder = Encoder(corpus_size, hidden_dim, seq_len, encoder_blocks, encoder_attention_heads, dropout, use_embedding, embedding_replacement)
+        self.decoder = Decoder(corpus_size, hidden_dim, seq_len, decoder_blocks, decoder_attention_heads, dropout, use_embedding, embedding_replacement)
 
     def encode(self, values):
         return self.encoder(values)
