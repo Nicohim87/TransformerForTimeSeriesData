6a26ff5328138fef9fb962cbb8a01742c881815e
Nicohim87 <nicohim8704@gmail.com>
2026-07-02 15:34:30 +0700

    feat: training preparation code (on preprocessing) and parameter file

--- preprocessing.py
+++ preprocessing.py
@@ -1,6 +1,14 @@
 import pandas as pd
 import unicodedata
+import joblib
+import json
 import re
+import os
+
+with open("parameters.json", 'r') as f:
+    params = json.load(f)
+
+# ---- Data Preparation ----
 
 # Read air quality dataset
 df = pd.read_csv("./data/global_air_quality_2014_2025.csv")
@@ -75,5 +83,85 @@ first_cols = ['City_id', 'Seq_id', 'Year', 'Month', 'Lat', 'Lon', 'Population_De
 
 df = df[first_cols + [c for c in df.columns if c not in first_cols]]
 
-# Save
+# ---- Save Processed Raw DS ----
 df.to_csv("./data/preprocessed_data.csv", index=False)
+
+
+
+# ---- Train Test Splitting ----
+from sklearn.model_selection import train_test_split
+import numpy as np
+
+SEQ_LEN = params["seq_len"]
+
+entities = df["City_id"].unique()
+
+# Split test only entities
+train_entities, test_entities = train_test_split(
+    entities,
+    test_size=params["test_only_entity"],
+    random_state=42
+)
+
+test_df  = df[df["City_id"].isin(test_entities)]
+df = df[df["City_id"].isin(train_entities)]
+
+# Get sequence starts
+entity = df["City_id"].unique()
+seq = df["Seq_id"].unique()
+seq_start = np.arange(seq.min(), seq.max() - SEQ_LEN + 1, 1)
+
+# Train, val, test split
+train_seq = seq_start[:- params["val_time_len"] - params["test_time_len"]]
+val_seq = seq_start[- params["val_time_len"] - params["test_time_len"]: - params["test_time_len"]]
+test_seq = seq_start[- params["test_time_len"]:]
+
+
+
+# ---- Normalization ----
+from sklearn.preprocessing import StandardScaler
+
+# Initiate scaler and scaler df
+scaler = StandardScaler()
+scaler_seq = set(seq[:- params["val_time_len"] - params["test_time_len"]])
+scaler_df = df[df["Seq_id"].isin(scaler_seq)].drop(columns=["City_id", "Seq_id"])
+
+# Fit scaler
+scaler.fit(scaler_df)
+
+# Scale the whole ds
+df[df.columns[2:]] = scaler.transform(df[df.columns[2:]])
+test_df[test_df.columns[2:]] = scaler.transform(test_df[test_df.columns[2:]])
+
+# ---- Entity - Seq Pair ----
+def generate_pair(seq, entity=entity):
+    pairs = []
+
+    for s in seq:
+        for e in entity:
+            pairs.append((e, s))
+
+    return pairs
+
+ds_train = generate_pair(train_seq)
+ds_val = generate_pair(val_seq)
+ds_test = generate_pair(test_seq)
+
+ds_test_only = generate_pair(seq_start, test_df["City_id"].unique())
+
+
+
+# ---- Save scaled ds, scaler, and pairs ----
+df.to_csv("./data/scaled_data.csv", index=False)
+test_df.to_csv("./data/scaled_test_data.csv", index=False)
+
+with open("./data/entity_seq_pair.json", "w") as f:
+    json.dump({
+        "train": ds_train,
+        "val": ds_val,
+        "test": ds_test,
+        "test_entities": ds_test_only
+    }, f, indent=4, default=int)
+
+os.makedirs("./model", exist_ok=True)
+joblib.dump(scaler, "model/scaler.pkl")
